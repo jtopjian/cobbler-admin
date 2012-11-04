420734d767d1e62d9f932808be8fb26f3f73a150
Joe Topjian <joe@topjian.net>
2012-11-03 23:17:33 -0600

    Changes

--- www/config/puppet-bootstrap.pp
+++ www/config/puppet-bootstrap.pp
@@ -1,10 +1,3 @@
-cron { 'puppet':
-  command => 'puppet agent --onetime --no-daemonize --logdest syslog > /dev/null 2>&1',
-  user    => 'root',
-  minute  => fqdn_rand(60),
-  ensure  => present,
-}
-
 # Add Puppet apt repo
 apt::source { 'puppet':
   location    => 'http://apt.terrarum.net/ubuntu',
